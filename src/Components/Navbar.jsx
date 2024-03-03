beabced6183a843ec02f050f5e7b76f2959e8626
sonu9346 <rahulbangle20@gmail.com>
2024-03-03 14:59:42 +0000

    Mar 3, 2024, 8:29 PM

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -20,6 +20,11 @@ const Navbar = () => {
 
   const toogleMenu = () => {
     setOpen((prev) => !prev);
+    if (open) {
+      console.log("open");
+    } else {
+      console.log("clis");
+    }
   };
 
   useMotionValueEvent(scrollY, "change", (latestValue) => {
@@ -43,7 +48,10 @@ const Navbar = () => {
         className="Navbar sticky top-0 z-[9999] flex  w-full items-center justify-between overflow-hidden px-4 py-2 backdrop-blur-sm md:px-12 md:py-4"
       >
         <Logo />
-        <Menu className="Hamburgur block cursor-pointer text-[6vw] sm:hidden" />
+        <Menu
+          className="Hamburgur block cursor-pointer text-[6vw] sm:hidden"
+          onClick={() => toogleMenu()}
+        />
         <motion.div className="hidden gap-8 lg:flex">
           {navLinks.map((e, i) => (
             <>
