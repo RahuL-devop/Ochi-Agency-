1a398d84aed91a9beead32a3c9b0d85c1bcadfcd
sonu9346 <rahulbangle20@gmail.com>
2024-03-03 09:45:18 +0000

    Mar 3, 2024, 3:15 PM

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -28,10 +28,12 @@ const Navbar = () => {
         className="Navbar sticky top-0 z-[9999] flex  w-full  items-center justify-between overflow-hidden  px-12  py-4 backdrop-blur-sm"
       >
         <Logo />
-        <motion.div className=" flex gap-8   
+        <motion.div
+          className=" flex gap-8   
         
         
-        ">
+        "
+        >
           {["Services", "Our Work", "About us ", "Insights", "Contact us"].map(
             (e, i) => (
               <>
@@ -39,19 +41,18 @@ const Navbar = () => {
                   <motion.a
                     className={`  Nav__linkk neue relative block cursor-pointer text-[16px] font-light leading-[12px] ${i === 4 && "ml-[12rem]"}`}
                     key={i}
-                    initial={{ y: 0 }}
                     whileHover={{
-                      translateY: "-20px",
+                      translateY: "20px",
                     }}
                   >
                     {e}
                   </motion.a>
 
                   <motion.a
-                    className={` Nav__link neue absolute top-0  translate-y-[125%] cursor-pointer text-[16px] font-light leading-[12px] ${i === 4 && "ml-[12rem]"}`}
+                    className={` Nav__link neue absolute top-0  translate-y-[125%] cursor-pointer text-[16px] font-light leading-[12px] text-red-500 ${i === 4 && "ml-[12rem]"}`}
                     key={i}
-                    initial={{ y: 20 }}
-                    whileHover={{ y: 0 }}
+                    initial={{ y: "0px" }}
+                    animate={{ y: "10px" }}
                   >
                     {e}
                   </motion.a>
