e8113e45b59600d3c632646c4abe3cf1ecd1784a
sonu9346 <rahulbangle20@gmail.com>
2024-03-03 08:54:23 +0000

    Mar 3, 2024, 2:24 PM

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -28,19 +28,21 @@ const Navbar = () => {
         className="Navbar sticky top-0 z-[9999] flex  w-full  items-center justify-between overflow-hidden  px-12  py-4 backdrop-blur-sm"
       >
         <Logo />
-        <motion.div className="relative flex gap-8 ">
+        <motion.div className=" flex gap-8 ">
           {["Services", "Our Work", "About us ", "Insights", "Contact us"].map(
             (e, i) => (
               <>
-                <motion.a
-                  className={`Nav__link neue  cursor-pointer text-[16px] font-light leading-[12px] ${i === 4 && "ml-[12rem]"}`}
-                  key={i}
-                >
-                  {e}
-                </motion.a>
+                <div className="relative">
+                  <motion.a
+                    className={`Nav__link neue block cursor-pointer text-[16px] font-light leading-[12px] ${i === 4 && "ml-[12rem]"}`}
+                    key={i}
+                  >
+                    {e}
+                  </motion.a>
+                </div>
 
                 <motion.a
-                  className={`Nav__link neue absolute left-0 top-0 cursor-pointer text-[16px] font-light leading-[12px] ${i === 4 && "ml-[12rem]"}`}
+                  className={`Nav__link neue absolute top-0  translate-y-[125%] cursor-pointer text-[16px] font-light leading-[12px] ${i === 4 && "ml-[12rem]"}`}
                   key={i}
                 >
                   {e}
