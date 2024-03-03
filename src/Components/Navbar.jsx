da23979610719bb7f546ed58ef17110deb9432cd
sonu9346 <rahulbangle20@gmail.com>
2024-03-03 04:29:33 +0000

    Mar 3, 2024, 9:59 AM

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Logo from "../assets/Loogo";
-// import Menu from "./Menu";
+import { motion, useMotionValueEvent, useScroll } from "framer-motion";
 
+// import Menu from "./Menu";
 const Navbar = () => {
   return (
     <>
