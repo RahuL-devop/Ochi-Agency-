23c9773d3c45def3b942c06146f2badb1fbbba8e
sonu9346 <rahulbangle20@gmail.com>
2024-03-03 14:25:30 +0000

    Mar 3, 2024, 7:55 PM

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -26,11 +26,11 @@ const Navbar = () => {
         }}
         animate={hidden ? "hidden" : "visible"}
         transition={{ duration: 0.35, ease: "easeInOut" }}
-        className="Navbar sticky top-0 z-[9999] flex  w-full  items-center justify-between overflow-hidden    px-4 py-2 backdrop-blur-sm md:px-12 md:py-4"
+        className="Navbar sticky top-0 z-[9999] flex  w-full  items-center justify-between overflow-hidden px-4 py-2 backdrop-blur-sm md:px-12 md:py-4"
       >
         <Logo />
-        <RiMenu3Fill className="block cursor-pointer  text-[6vw] md:block" />
-        <motion.div className="hidden gap-8 md:flex">
+        <RiMenu3Fill className="block cursor-pointer  text-[6vw] lg:hidden" />
+        <motion.div className="hidden gap-8 lg:flex">
           {["Services", "Our Work", "About us ", "Insights", "Contact us"].map(
             (e, i) => (
               <>
