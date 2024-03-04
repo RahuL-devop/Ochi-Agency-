776eb2129ea9a29954252c17e2a74d081dec2ed4
sonu9346 <rahulbangle20@gmail.com>
2024-03-04 02:05:23 +0000

    Mar 4, 2024, 7:35 AM

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -4,12 +4,7 @@ import { FaLongArrowAltUp } from "react-icons/fa";
 const Landing = () => {
   return (
     <>
-      <div
-        // data-scroll
-        // data-scrol-section
-        // data-scroll-speed="-0.3"
-        className="min-h-[100vh] w-full bg-zinc-900 py-[1px]"
-      >
+      <div className="min-h-[100vh] w-full bg-zinc-900 py-[1px]">
         <div className=" textStructure mt-10 px-4 md:mt-[10.5rem] md:px-12">
           {["We create", "eye-opening", "Presentations"].map((e, i) => {
             return (
