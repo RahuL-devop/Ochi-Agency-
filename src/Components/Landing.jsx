c1063362f32ea86a225254d05a838855909cf5ef
sonu9346 <rahulbangle20@gmail.com>
2024-03-04 13:12:01 +0000

    Mar 4, 2024, 6:42 PM

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -15,7 +15,7 @@ const Landing = () => {
                       initial={{ width: 0 }}
                       animate={{ width: "13vw" }}
                       transition={{ duration: 1, ease: [0.87, 0, 0.13, 1] }}
-                      className="relative -top-[1.23vw]   mr-[0.5vw] h-[11.5vw] w-[12vw] rounded-xl bg-[url('https://ochi.design/wp-content/uploads/2022/04/content-image01.jpg')] bg-cover bg-center md:h-[6.5vw] "
+                      className="relative -top-[1.23vw] mr-[0.5vw] h-[11.5vw] w-[12vw] rounded-xl bg-[url('https://ochi.design/wp-content/uploads/2022/04/content-image01.jpg')] bg-cover bg-center md:h-[6.5vw] "
                     ></motion.div>
                   )}
                   <h1
